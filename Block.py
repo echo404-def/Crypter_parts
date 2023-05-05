0a0aa6d9d3236f03402fb356e41bf496afe6dace
MHP <85354497+echo404-def@users.noreply.github.com>
2023-05-05 19:38:12 +0900

    Add files via upload

--- Block.py
+++ Block.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Apr 30 17:55:54 2023
 
-@author: user
+@author: echo
 """
 
 class Block:
@@ -11,5 +11,18 @@ class Block:
             m = kwargs["multiple"]
         else:
             m = 10
-        
-        
+        if len(data) % m != 0:
+            print("\aERROR[Block]: Incorrect multiple value")
+            return()
+        res = list()
+        for i in range(len(data)//m):
+            i = data[:10]
+            data = data[10:]
+            res.append(i)
+        return(res)
+            
+if __name__ == "__main__":
+    data = list(range(30))
+    r = Block().blocking(data)
+    print(r)
+    
